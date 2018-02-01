a8248b170b5fe506ad397e90a65640436cd13467
Stabz <dmit0038@edu.easj.dk>
2018-02-01 09:29:20 +0100

    adding the game
    the first

---
+++ Assets/scripts/Platformspawn.cs
@@ -0,0 +1,64 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Platformspawn : MonoBehaviour {
+    public float maxLeft = -4.5f;
+    public float maxRight = 4.5f;
+
+    public float randomPlatDist = 2f;
+    public float PlatDist = 1.2f;
+    public float desiredDist = 7f;
+
+    private float playerHighestPos = 0;
+    private float latestPlatHeight = 0;
+
+
+    private GameObject player;
+
+    private List<GameObject> blockList = new List<GameObject>();
+
+
+    // Use this for initialization
+    void Start () {
+        player = GameObject.FindGameObjectWithTag("Player");
+        
+        createPlatform(4f);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+       
+
+        if (player.transform.position.y > playerHighestPos)
+        {
+            playerHighestPos = player.transform.position.y;
+        }
+
+        if(playerHighestPos > latestPlatHeight - desiredDist)
+        {
+
+            createPlatform(latestPlatHeight + PlatDist);
+        }
+		
+	}
+
+   private void deletePlatform(float block)
+    {
+        
+    }
+
+    private void createPlatform(float height)
+    {
+        float x = Random.Range(maxLeft, maxRight);
+        float y = Random.Range(-randomPlatDist, +randomPlatDist);
+        GameObject enemyInstance = Instantiate(Resources.Load("block", typeof(GameObject))) as GameObject;
+        enemyInstance.transform.Translate(new Vector3(x, height + y, 0));
+        enemyInstance.transform.parent = transform;
+
+        latestPlatHeight = height;
+
+        Destroy(enemyInstance, 10f);
+        
+    }
+}
---
+++ Assets/scripts/jump.cs
@@ -0,0 +1,82 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class jump : MonoBehaviour {
+
+    // Use this for initialization
+    private Rigidbody2D jumRigBody;
+
+    public Sprite falling, jumping;
+
+
+    public float maxLeft = -4f;
+    public float maxRight = 4f;
+
+    public float horizontalSpeed = 0.3f;
+    public float jumpSpeed = 400f;
+
+    private float lastKnownPos = 0f;
+    private float lookingWay = 0.5809383f;
+    
+    void Start () {
+        jumRigBody = GetComponent<Rigidbody2D>();
+
+    }
+	
+	// Update is called once per frame
+	void Update () {
+
+
+        Vector3 scale = transform.localScale;
+
+
+        if (Input.GetKey(KeyCode.LeftArrow) && transform.position.x > maxLeft)
+        {
+            
+            scale.x = -lookingWay;
+            transform.Translate(Vector3.left * horizontalSpeed * Time.deltaTime);
+        }
+
+        if (Input.GetKey(KeyCode.RightArrow) && transform.position.x < maxRight)
+        {
+            scale.x = lookingWay;
+            transform.Translate(Vector3.right * horizontalSpeed * Time.deltaTime);
+        }
+
+        fallingOrJumping();
+
+        transform.localScale = scale; 
+
+    }
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+
+        if (other.tag == "platform")
+        {
+            Debug.Log("jump");
+            jumRigBody.velocity = Vector3.zero;
+            jumRigBody.AddForce(Vector3.up * jumpSpeed);
+        }
+        else
+        {
+            Debug.Log("player colides with something other than a platform that also has a colider");
+        }
+    }
+
+
+    private void fallingOrJumping()
+    {
+        if (lastKnownPos > transform.position.y)
+        {
+            lastKnownPos = transform.position.y;
+            this.GetComponent<SpriteRenderer>().sprite = falling;
+        }
+        
+        if(lastKnownPos < transform.position.y)
+        {
+            lastKnownPos = transform.position.y;
+            this.GetComponent<SpriteRenderer>().sprite = jumping;
+        }
+    }
+}
